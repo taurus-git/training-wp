1885d8b9ca2c05f8caf24409870785c3e0aaa5f9
taurus-git <t.1113@ya.ru>
2019-02-04 15:55:37 +0200

    The method to view "Breed" was changed.

--- template-parts/post/animals-post-format.php
+++ template-parts/post/animals-post-format.php
@@ -8,14 +8,7 @@
     <?php the_field('description'); ?>
 </div>
 <div>
-    <?php
-    $cur_terms = get_the_terms($post->ID, 'dogscat');
-    if (is_array($cur_terms)) {
-        foreach ($cur_terms as $cur_term) {
-            echo '<b>Breed:</b> ' . '<a href="' . get_term_link($cur_term->term_id, $cur_term->taxonomy) . '">' . $cur_term->name . '</a>';
-        }
-    }
-    ?>
+    <?php the_terms( $post->ID, 'dogscat', '<b>Breed: </b>'); ?>
 </div>
 <div>
     <b>Age: </b><?php the_field('age'); ?>
