d3f6e3d0de629861c4d3e764a1cfa94a9a2cf48c
taurus-git <t.1113@ya.ru>
2019-02-04 09:02:24 +0200

    Reformat code.

--- single.php
+++ single.php
@@ -16,21 +16,21 @@ get_header(); ?>
             <main id="main" class="site-main" role="main">
                 <?php
                 /* Start the Loop */
-                while ( have_posts() ) : the_post();
-                if( is_singular( array('dogs', 'cats') ) ){
-                    get_template_part( 'template-parts/post/animals-post-format', get_post_format() );
+                while (have_posts()) : the_post();
+                    if (is_singular(array('dogs', 'cats'))) {
+                        get_template_part('template-parts/post/animals-post-format', get_post_format());
+                    } else {
+                        get_template_part('template-parts/post/content', get_post_format());
                     }
-                else { get_template_part( 'template-parts/post/content', get_post_format() ); }
-
                     // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
+                    if (comments_open() || get_comments_number()) :
                         comments_template();
                     endif;
 
-                    the_post_navigation( array(
-                        'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-                        'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-                    ) );
+                    the_post_navigation(array(
+                        'prev_text' => '<span class="screen-reader-text">' . __('Previous Post', 'twentyseventeen') . '</span><span aria-hidden="true" class="nav-subtitle">' . __('Previous', 'twentyseventeen') . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg(array('icon' => 'arrow-left')) . '</span>%title</span>',
+                        'next_text' => '<span class="screen-reader-text">' . __('Next Post', 'twentyseventeen') . '</span><span aria-hidden="true" class="nav-subtitle">' . __('Next', 'twentyseventeen') . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg(array('icon' => 'arrow-right')) . '</span></span>',
+                    ));
 
                 endwhile; // End of the loop.
                 ?>
